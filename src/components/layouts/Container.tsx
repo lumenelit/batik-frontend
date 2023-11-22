5a1e232864c1429dda6764229aa2fcd4c0b1af2d
Sitouxz <yehezkielowen0901@gmail.com>
2023-11-23 05:52:53 +0800

    Refactor Container component to add background images

--- src/components/layouts/Container.tsx
+++ src/components/layouts/Container.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import bgLeft from "../../assets/images/bg-left.png";
+import bgRight from "../../assets/images/bg-right.png";
 
 export default function Container({ children, center = true }) {
-    // return <div className='container px-4 mx-auto max-w-5xl'>{children}</div>;
-    if (!center) {
-        return <div className="container px-4 mx-[5%]">{children}</div>;
-    } else {
-        return <div className="container px-4 mx-auto">{children}</div>;
-    }
+    // return <div className='container max-w-5xl px-4 mx-auto'>{children}</div>;
+    return (
+        <>
+            <div className="fixed z-0 w-full h-screen overflow-hidden">
+                <img
+                    src={bgLeft}
+                    alt="bg"
+                    className="absolute bottom-0 left-0"
+                />
+                <img
+                    src={bgRight}
+                    alt="bg"
+                    className="absolute top-0 right-0"
+                />
+            </div>
+            <div
+                className={`container px-4 ${center ? "mx-auto" : "mx-[5%]"} `}
+            >
+                {children}
+            </div>
+        </>
+    );
 }
