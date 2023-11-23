97290444c38c0244c99c8905d53b286c7869a66f
MG177 <mg.terok77@gmail.com>
2023-11-23 09:41:29 +0800

    modify api timeout

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,12 +1,12 @@
 // create axios
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  timeout: 5000,
-  headers: {
-    'Content-Type': 'application/json'
-  }
+    baseURL: process.env.REACT_APP_API_URL,
+    timeout: 50000,
+    headers: {
+        "Content-Type": "application/json"
+    }
 });
 
 export default api;
